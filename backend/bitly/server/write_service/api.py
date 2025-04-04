1f4197928b07a0137239d3d2cc7443915ff1b5aa
ktjayamanna <kaveen.jayamanna@gmail.com>
2025-04-04 12:25:42 -0400

    some cleanup and added sentry

--- backend/bitly/server/write_service/api.py
+++ backend/bitly/server/write_service/api.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 from .health import router as health_router
 from .core import router as core_router
+import sentry_sdk
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from dotenv import load_dotenv
+import os
+
+load_dotenv('backend/.vscode/.env')
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    send_default_pii=True,
+    integrations=[
+        FastApiIntegration(
+            transaction_style=os.getenv("SENTRY_TRANSACTION_STYLE", "url"),  # Use URL path as transaction name
+        ),
+    ],
+)
 
 app = FastAPI(title="Write Microservice", version="1.0.0")
 
--- backend/scripts/bash/client_requests.sh
+++ backend/scripts/bash/client_requests.sh
@@ -2,4 +2,4 @@
 
 curl -X POST "http://localhost:8000/urls" \
      -H "Content-Type: application/json" \
-     -d '{"original_url": "https://www.fooliobar.com", "user_id": 3}'
+     -d '{"original_url": "https://www.ycombinator.com", "user_id": 3}'
